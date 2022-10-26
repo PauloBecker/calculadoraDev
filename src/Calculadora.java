aef7cd6a75e8b8be61c4e98fb386576beb7b1c8d
Paulo Becker <beckerpaulo81@gmail.com>
2022-10-26 20:43:30 -0300

    Implementado um laço de repetição e um menu de escolha para continuar ou encerrar o sistema

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class Calculadora {
-
-    Scanner tc = new Scanner(System.in);
     public void init(){
+
+
+        String opc = "";
+
+        while (!opc.equalsIgnoreCase("N")){
 
             System.out.println("Informe o calculo desejado!");
             String txt = tc.nextLine();
@@ -12,25 +15,37 @@ public class Calculadora {
             float valor1 = Float.parseFloat(operacao[0]);
             float valor2 = Float.parseFloat(operacao[2]);
 
+            switch (operacao[1]){
+                case "+":
+                    System.out.println(txt + " = " + this.adicao(valor1, valor2));
+                    break;
+                case "-":
+                    System.out.println(txt + " = " + this.subtracao(valor1,valor2));
+                    break;
+                case "*":
+                    System.out.println(txt + " = " + this.multiplicacao(valor1,valor2));
+                    break;
+                case "/":
+                    System.out.println(txt + " = " + this.divisao(valor1,valor2));
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+            System.out.println("Deseja fazer outra operação? \n" +
+                    "Digite [S] para continuar: " +
+                    "\nDigite [N] para encerrar o sistema: "
+                    );
+            opc = tc.nextLine();
+        }
 
-                    switch (operacao[1]){
-                        case "+":
-                            System.out.println(txt + " = " + this.adicao(valor1, valor2));
-                            break;
-                        case "-":
-                            System.out.println(txt + " - " + this.subtracao(valor1,valor2));
-                            break;
-                        case "*":
-                            System.out.println(txt + " / " + this.multiplicacao(valor1,valor2));
-                            break;
-                        case "/":
-                            System.out.println(txt + " / " + this.divisao(valor1,valor2));
-                            break;
-                        default:
-                            System.out.println("Opção inválida!");
-                            break;
-                    }
-                }
+        System.out.println("Encerrando o sistema...\n" +
+                "Até logo!");
+
+    }
+
+
+    Scanner tc = new Scanner(System.in);
 
 
     public float adicao(float v1, float v2){
